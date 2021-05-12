6ac7b75830ec5b49970c0f54434764be3c56de6e
Joshua Zenn <wordman05@gmail.com>
2021-05-11 23:08:48 -0400

    Initial project upload

---
+++ MBB-Economy/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace MBB_Economy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Item = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BuyLocation = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BuyPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SellLocation = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SellPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.rd_Item = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.rd_AvgBuy = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.rd_AvgSell = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.rd_AvgNetGain = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 424);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Trades";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 424);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Item,
+            this.BuyLocation,
+            this.BuyPrice,
+            this.SellLocation,
+            this.SellPrice});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(786, 418);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.RowLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_RowLeave);
+            // 
+            // Item
+            // 
+            this.Item.HeaderText = "Item";
+            this.Item.Name = "Item";
+            // 
+            // BuyLocation
+            // 
+            this.BuyLocation.HeaderText = "Buy Location";
+            this.BuyLocation.Name = "BuyLocation";
+            // 
+            // BuyPrice
+            // 
+            this.BuyPrice.HeaderText = "Buy Price";
+            this.BuyPrice.Name = "BuyPrice";
+            // 
+            // SellLocation
+            // 
+            this.SellLocation.HeaderText = "Sell Location";
+            this.SellLocation.Name = "SellLocation";
+            // 
+            // SellPrice
+            // 
+            this.SellPrice.HeaderText = "Sell Price";
+            this.SellPrice.Name = "SellPrice";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.rd_Item,
+            this.rd_AvgBuy,
+            this.rd_AvgSell,
+            this.rd_AvgNetGain});
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(786, 418);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // rd_Item
+            // 
+            this.rd_Item.HeaderText = "Item";
+            this.rd_Item.Name = "rd_Item";
+            this.rd_Item.ReadOnly = true;
+            // 
+            // rd_AvgBuy
+            // 
+            this.rd_AvgBuy.HeaderText = "Avg Buy";
+            this.rd_AvgBuy.Name = "rd_AvgBuy";
+            this.rd_AvgBuy.ReadOnly = true;
+            // 
+            // rd_AvgSell
+            // 
+            this.rd_AvgSell.HeaderText = "Avg Sell";
+            this.rd_AvgSell.Name = "rd_AvgSell";
+            this.rd_AvgSell.ReadOnly = true;
+            // 
+            // rd_AvgNetGain
+            // 
+            this.rd_AvgNetGain.HeaderText = "Avg Net Gain";
+            this.rd_AvgNetGain.Name = "rd_AvgNetGain";
+            this.rd_AvgNetGain.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Item;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BuyLocation;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BuyPrice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SellLocation;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SellPrice;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rd_Item;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rd_AvgBuy;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rd_AvgSell;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rd_AvgNetGain;
+    }
+}
+
---
+++ MBB-Economy/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MBB_Economy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_RowLeave(object sender, DataGridViewCellEventArgs e)
+        {
+            recalculateRDTable();
+        }
+
+        private void recalculateRDTable()
+        {
+            var queryAvgBuy =
+                from transaction in dataGridView1.Rows.Cast<DataGridViewRow>()
+                select transaction;
+
+            dataGridView2.Rows.Clear();
+            foreach (DataGridViewRow row in queryAvgBuy)
+            {
+                if(row.Cells[0].Value != "")
+                {
+                    dataGridView2.Rows.Add(row.Cells[0].Value);
+                }
+            }
+        }
+    }
+}
